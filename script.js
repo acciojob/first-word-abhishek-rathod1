0ba4658cfbbbed00972e845ee92629279a4261ee
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-04-13 22:37:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function firstWord(s) {
   // your code here
-	let res = s.split(" ")
+	let res = s.trim().split(" ")
 	return res[0];
 	
 }
 
 // Do not change the code belowf
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
